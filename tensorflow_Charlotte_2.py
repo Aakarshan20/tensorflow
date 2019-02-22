f5dd550f7e3024fc4141da1302af72f47e4f4d64
aabbcc <iiooiio@hotmail.com>
2019-02-22 18:00:27 +0800

    update 20190222

--- tensorflow_Charlotte_2.py
+++ tensorflow_Charlotte_2.py
@@ -6,12 +6,17 @@ rng = numpy.random
 #參數設定
 learning_rate = 0.01
 training_epochs = 10000#迭代次數
-display_stop = 50#每隔50次迭代 會打印出數字
+#display_step = 50#每隔50次迭代 會打印出數字
+display_step = 200#每隔200次迭代 會打印出數字
 
 #訓練模型(用來預測的樣本)
+
 train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.197,0.42,10.791,5.313,7.997,5.654,9.27,3.1])
 train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.336,2.596,2.53,1.2,2.827,3.465,1.65,2.904,2.42,2.94,1.3])
-
+'''
+train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,7.042,10.791,5.313,7.997,5.654,9.27,3.1])
+train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+'''
 n_samples = train_X.shape[0]
 
 print("train_X:", train_X)
@@ -37,7 +42,8 @@ cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
 
 #梯度下降
 #注意 minimize() 可以修正W 和 b 因為默認設置Variables的trainable=True
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+#optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
 #初始化所有variables
 init = tf.global_variables_initializer()
@@ -48,11 +54,61 @@ with tf.Session() as sess:
 
     #灌入所有訓練數據(樣本)
     for epoch in range(training_epochs):
-        for (x,y) in zip(train_X, train_Y):#zip 打包可迭代的list 一一對應且元素個數以最短的為準
+        for (x,y) in zip(train_X, train_Y):#zip 打包可迭代的list 成為一個字典 內容一一對應且元素個數以最短的為準
             sess.run(optimizer, feed_dict={X:x, Y:y})
 
-            #打印出每次迭代的log
-            if(epoch+1) % display_step == 0:
+        #打印出每次迭代的log
+        if(epoch+1) % display_step == 0:
+            c = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+            print("Epoch:", "%04d" % (epoch+1), "cost=", "{:.9f}".format(c), "W=", sess.run(W), "b=", sess.run(b))
+    print("Optimization Finished!")
+    training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+    print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+	
+    #作圖
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
+	
+    #測試樣本
+    test_X = numpy.asarray([6.83,4.668,8.9,7.91,5.7,8.7,3.1,2.1])
+    test_Y = numpy.asarray([1.84,2.273,3.2,2.831,2.92,3.24,1.35,1.03])
+	
+    print("Testing...(Mean square loss Comparision)")
+    testing_cost = sess.run(
+        tf.reduce_sum(tf.pow(pred-Y,2)) / (2*test_X.shape[0]),
+        feed_dict={X:test_X, Y:test_Y})# same function as cost above
+    print("Testing cost=", testing_cost)
+    print("Absolute mean square loss difference:", abs(training_cost-testing_cost))
+
+    plt.plot(test_X, test_Y, 'bo', label='Testing data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted_line')
+    plt.legend()
+    plt.show()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+					
 
 
 
---
+++ tensorflow_Charlotte_2_MNIST.py
@@ -0,0 +1,77 @@
+#手寫數字識別
+import tensorflow as tf
+
+#加載mnist 數據集
+from tensorflow.examples.tutorials.mnist import input_data
+#mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+print(mnist)
+
+#設置參數
+learning_rate = 0.01
+training_epochs = 25
+batch_size = 100
+display_step = 1
+
+# tf Graph輸入
+x = tf.placeholder(tf.float32, [None, 784])#mnist的圖片大小
+y = tf.placeholder(tf.float32, [None, 10])
+
+#設定權重和偏移
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+#設定運行模型
+pred = tf.nn.softmax(tf.matmul(x, W)+b) #使用softmax來預測
+
+#設置cost function為交叉熵
+cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
+
+#梯度下降
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+#初始化權重
+init = tf.global_variables_initializer()
+
+#開始訓練
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(mnist.train.num_examples/batch_size)
+
+        #遍歷每個batch
+        for i in range(total_batch):
+            
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+
+            #把每個batch的值放下去訓練
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs, y: batch_ys})
+
+            #計算平均損失
+            avg_cost += c/total_batch
+            
+        #印出每次迭代的日誌
+        if (epoch+1) % display_step ==0:
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+                  
+    print("Optimization Finished!")
+
+    #測試模型
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+
+    #計算3000個樣本的準確率
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print("Accuracy", accuracy.eval({x:mnist.test.images[:3000], y: mnist.test.labels[:3000]}))
+
+
+
+
+
+
+
+
+
